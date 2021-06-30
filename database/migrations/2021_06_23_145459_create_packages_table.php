9e94cbc0fc49b9d1fb81abee9da43ebf78dd14aa
Iruiasaro <brunettirosario@gmail.com>
2021-06-30 17:05:02 +0200

    fix table

--- database/migrations/2021_06_23_145459_create_packages_table.php
+++ database/migrations/2021_06_23_145459_create_packages_table.php
@@ -16,6 +16,13 @@ class CreatePackagesTable extends Migration
         Schema::create('packages', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name', 50)->nullable();
+            $table->longText('description');
+            $table->string('city', 50);
+            $table->decimal('price', $precision = 5, $scale = 2);
+            $table->date("departure");
+            $table->date("arrival");
+            $table->integer('nights')->nullable();
             
         });
     }
